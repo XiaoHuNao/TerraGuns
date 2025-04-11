5b51d082b854532b99b92ffebf7e9078b8489014
westernat <westernat233@163.com>
2025-04-11 13:28:11 +0800

    移除所有不能运行的代码

--- src/main/java/org/confluence/terra_guns/event/GameEvent.java
+++ src/main/java/org/confluence/terra_guns/event/GameEvent.java
@@ -1,10 +1,5 @@
 package org.confluence.terra_guns.event;
 
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import org.confluence.terra_guns.TerraGuns;
-
-@EventBusSubscriber(modid = TerraGuns.MODID, bus = EventBusSubscriber.Bus.GAME)
+//@EventBusSubscriber(modid = TerraGuns.MODID, bus = EventBusSubscriber.Bus.GAME)
 public class GameEvent {
 }
--- src/main/java/org/confluence/terra_guns/event/ModEvent.java
+++ src/main/java/org/confluence/terra_guns/event/ModEvent.java
@@ -1,9 +1,5 @@
 package org.confluence.terra_guns.event;
 
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.common.EventBusSubscriber;
-import org.confluence.terra_guns.TerraGuns;
-
-@EventBusSubscriber(modid = TerraGuns.MODID, bus = EventBusSubscriber.Bus.MOD)
+//@EventBusSubscriber(modid = TerraGuns.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ModEvent {
 }
