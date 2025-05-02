2fe53c7f7f83bf9f352906ff4c19c938ed2bd9f8
westernat <westernat233@163.com>
2025-05-02 15:53:53 +0800

    修复幸运药水的半透明图标在背包中失效

--- src/main/java/org/confluence/terra_guns/client/event/ModEvent.java
+++ src/main/java/org/confluence/terra_guns/client/event/ModEvent.java
@@ -7,21 +7,20 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
-import net.neoforged.neoforge.registries.DeferredItem;
 import org.confluence.terra_guns.TerraGuns;
 import org.confluence.terra_guns.client.renderer.entity.BulletRenderer;
 import org.confluence.terra_guns.common.init.TGEntities;
 import org.confluence.terra_guns.common.init.TGItems;
-import org.confluence.terra_guns.common.item.gun.BaseGun;
 import org.confluence.terra_guns.util.TGUtil;
 
-import static org.confluence.terra_guns.client.init.TGKeys.*;
+import static org.confluence.terra_guns.client.init.TGKeys.AIM;
+import static org.confluence.terra_guns.client.init.TGKeys.SHOOT;
 
 @EventBusSubscriber(modid = TerraGuns.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModEvent {
     @SubscribeEvent
     public static void registerClientExtensions(RegisterClientExtensionsEvent event) {
-        TGItems.GUNS.getEntries().forEach(holder -> TGUtil.registerGunModel(event, (DeferredItem<? extends BaseGun>) holder));
+        TGItems.GUNS.getEntries().forEach(holder -> TGUtil.registerGunModel(event, holder));
     }
 
     @SubscribeEvent
--- src/main/java/org/confluence/terra_guns/util/TGUtil.java
+++ src/main/java/org/confluence/terra_guns/util/TGUtil.java
@@ -2,8 +2,9 @@ package org.confluence.terra_guns.util;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
-import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import org.confluence.terra_guns.TerraGuns;
 import org.confluence.terra_guns.client.renderer.item.SimpleGeoItemRenderer;
 import org.confluence.terra_guns.common.item.gun.BaseGun;
@@ -11,15 +12,15 @@ import software.bernie.geckolib.model.DefaultedItemGeoModel;
 
 
 public class TGUtil {
-    public static void registerOtherGunModel(RegisterClientExtensionsEvent event, String modid, DeferredItem<? extends BaseGun> gunSupplier) {
+    public static void registerOtherGunModel(RegisterClientExtensionsEvent event, String modid, DeferredHolder<Item, ? extends Item> gunSupplier) {
         registerGunModel(event, ResourceLocation.fromNamespaceAndPath(modid, "gun/" + gunSupplier.getId().getPath()), gunSupplier);
     }
 
-    public static void registerGunModel(RegisterClientExtensionsEvent event, DeferredItem<? extends BaseGun> gunSupplier) {
+    public static void registerGunModel(RegisterClientExtensionsEvent event, DeferredHolder<Item, ? extends Item> gunSupplier) {
         registerGunModel(event, ResourceLocation.fromNamespaceAndPath(TerraGuns.MODID, "gun/" + gunSupplier.getId().getPath()), gunSupplier);
     }
 
-    public static void registerGunModel(RegisterClientExtensionsEvent event, ResourceLocation resourceLocation, DeferredItem<? extends BaseGun> gunSupplier) {
+    public static void registerGunModel(RegisterClientExtensionsEvent event, ResourceLocation resourceLocation, DeferredHolder<Item, ? extends Item> gunSupplier) {
         event.registerItem(new SimpleGeoItemRenderer<BaseGun>(new DefaultedItemGeoModel<>(resourceLocation)), gunSupplier.get());
     }
 
