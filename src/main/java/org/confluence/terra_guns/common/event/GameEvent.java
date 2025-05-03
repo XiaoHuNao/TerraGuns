f268a3d4f1d8533479372ce9ee74361be6d40fb4
KL_jiana <2436262516@qq.com>
2025-05-03 21:05:54 +0800

    🖊️安全强转

--- src/main/java/org/confluence/terra_guns/common/event/GameEvent.java
+++ src/main/java/org/confluence/terra_guns/common/event/GameEvent.java
@@ -18,14 +18,16 @@ import org.confluence.terra_guns.common.item.gun.BaseGun;
 public class GameEvent {
     @SubscribeEvent
     public static void swapGunAnimator(LivingEquipmentChangeEvent event){
-        if (event.getSlot() == EquipmentSlot.MAINHAND && event.getTo().getItem() instanceof BaseGun baseGun) {
-            // 切枪
-            baseGun.pickAnimator(event.getTo(), (ServerPlayer) event.getEntity());
+        if (event.getEntity() instanceof ServerPlayer serverPlayer){
+            if (event.getSlot() == EquipmentSlot.MAINHAND && event.getTo().getItem() instanceof BaseGun baseGun) {
+                // 切枪
+                baseGun.pickAnimator(event.getTo(), serverPlayer);
 //            TerraGuns.LOGGER.info(event.getTo().toString());
-        }
-        if (event.getSlot() == EquipmentSlot.MAINHAND && event.getFrom().getItem() instanceof BaseGun) {
-            // 收枪
+            }
+            if (event.getSlot() == EquipmentSlot.MAINHAND && event.getFrom().getItem() instanceof BaseGun) {
+                // 收枪
 //            TerraGuns.LOGGER.info(event.getFrom().toString());
+            }
         }
     }
 
@@ -36,6 +38,4 @@ public class GameEvent {
             event.setInvulnerabilityTicks(0);
         }
     }
-
-
 }
