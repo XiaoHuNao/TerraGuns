2d4912ba032e178e300092c664294a884df85f69
KalebLangley <2436262516@qq.com>
2025-05-23 18:39:41 +0800

    🔧允许持枪交互

--- src/main/java/org/confluence/terra_guns/common/event/GameEvent.java
+++ src/main/java/org/confluence/terra_guns/common/event/GameEvent.java
@@ -1,6 +1,9 @@
 package org.confluence.terra_guns.common.event;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageSources;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
